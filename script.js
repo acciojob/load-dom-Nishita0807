7c8e429a83dc30229b8c699d9a40a87aa0c1438e
Nishita Rani <92663360+Nishita0807@users.noreply.github.com>
2023-09-18 15:30:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 //your JS code here. If required.
+// Function to run when the DOM is loaded
+function onDomContentLoaded() {
+  // Create a new paragraph element
+  var paragraph = document.createElement("p");
+
+  // Set the text content of the paragraph
+  paragraph.textContent = "DOM load success";
+
+  // Append the paragraph to the body
+  document.body.appendChild(paragraph);
+}
+
+// Add an event listener for the DOMContentLoaded event
+document.addEventListener("DOMContentLoaded", onDomContentLoaded);
